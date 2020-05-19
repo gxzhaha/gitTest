252fad5719d89d2e3a43c154fc10d4ea7c46bc34
Gao Xiaozhuang <1986959053@qq.com>
2020-05-19 20:50:27 +0800

    第一次提交本地工程

---
+++ git_test/src/com/itheima/first.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author 晓庄哥哥
+ * @warning 要写注释弟弟
+ **/
+public class first {
+    public static void main(String[] args) {
+        System.out.println("get_idea");
+    }
+}
